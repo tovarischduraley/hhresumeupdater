88c58f2788036c5a3ea5a9784a80d46721007a79
Vladimir Stepanov <vovastepanov777@gmail.com>
2022-09-16 03:34:34 +0300

    Refactored app structure

--- hh.py
+++ hh.py
@@ -1,4 +1,6 @@
 import os
+from time import sleep
+from typing import Optional
 import requests
 from dotenv import load_dotenv
 
@@ -26,7 +28,13 @@ class HHResumeUpdater(metaclass=Singleton):
     Implement get_code()
     """
 
-    def get_access_token(self):
+    def get_code(self):
+        url = f"https://hh.ru/oauth/authorize?response_type=code&client_id={self.client_id}"
+        response = requests.get(url=url)
+        print("Response text: \n", response.text)
+        print("Response url: \n", response.url)
+
+    def authenticate(self) -> dict:
         url = "https://hh.ru/oauth/token"
 
         response = requests.post(url=url, data={"grant_type": "authorization_code",
@@ -36,25 +44,33 @@ class HHResumeUpdater(metaclass=Singleton):
 
         if response.status_code == 200:
             return response.json()
-            # return response.json()
+
         print("Response text", response.text)
         print("Response code", response.status_code)
 
+    def update_resume(self, access_token: str):
+        url = f"https://api.hh.ru/resumes/{self.resume_id}/publish"
+        headers = {'Authorization': 'Bearer ' + access_token}
 
-updater = HHResumeUpdater()
+        def update(sleep_time: Optional[int] = 0) -> None:
+            sleep(sleep_time)
+            response = requests.post(url=url, headers=headers)
+            if response.status_code == 200:
+                print('Success: ', 'Resume was updated')
+                update(60 * 60 * 4 + 60 * 1)
+            if response.status_code == 429:
+                print('Error: ', 'You are trying to update your resume too often')
+                update(10 * 60)
+            if response.status_code == 403:
+                print('Error: ', 'Token TTL has expired. Get a new token')
+                exit(1)
 
-# https://hh.ru/oauth/authorize?response_type=code&client_id=CLIENT_ID
-#
-# copy code from this http://yourapphost/?code=CODE
-#
-# curl -k -X POST -H 'User-Agent: api-test-agent' -d 'grant_type=authorization_code&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&code=CODE' https://hh.ru/oauth/token
-#
-# resp = {
-#     "token_type": "bearer",
-#     "access_token": "",
-#     "refresh_token": "UKK5I63M7T65H21K14DS2DPCSUG2HDSNLQ6407S5HD206KSH9PJRAVLVSAOR2RD7",
-#     "expires_in": 1209599  # = 2 weeks
-# }
+        update()
+
 
 if __name__ == '__main__':
-    updater.get_access_token()
+    updater = HHResumeUpdater()
+    try:
+        updater.get_code()
+    except Exception as e:
+        print(f"Caught Exception: {e}")
